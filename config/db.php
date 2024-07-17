7d3cd8fd9e5c12a888335cd32270db5a14984279
Xandalee-W-M <musunguxandalee@gmail.com>
2024-07-17 23:44:14 +0300

    update

--- config/db.php
+++ config/db.php
@@ -17,6 +17,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 };
+
 function createTables($conn) {
     // SQL to create tables
     $sql = "
